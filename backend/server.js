43e587eba4b81544ceb4b2c626b0361d4816e7f6
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-06-24 15:08:11 +0200

    feat: Aktualisiere Skripte in package.json und füge API-Endpunkte in server.js hinzu

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,22 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 
+app.get('/api/notes', (req, res) => {
+  res.send('This is the notes API endpoint.');
+});
+
+app.post('/api/notes', (req, res) => {
+  res.status(201).json({message:'Note created successfully.'});
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  res.status(200).json({message:'Note updated successfully.'});
+});
+
+app.delete('/api/notes/id', (req, res) => {
+  res.status(200).json({message:'Note deleted successfully.'});
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });

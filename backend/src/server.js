8416022b76342de28c952499f073d95352220365
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-06-24 15:50:55 +0200

    feat: Aktualisiere Skripte in package.json und implementiere Notizen-API mit Routen und Controller

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import notesRoutes from './routes/notesRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
+app.use('/api/notes', notesRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

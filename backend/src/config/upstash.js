61dd6132471416f0e07ca8d7ac5ab0f41e8d92f2
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-06-30 20:02:59 +0200

    feat: Entferne dotenv-Abhängigkeit und lade Umgebungsvariablen direkt aus Redis

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,8 +1,5 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
-import dotenv from "dotenv";
-
-dotenv.config(); // Load environment variables from .env file
 
 const rateLimit = new Ratelimit({
   redis: Redis.fromEnv(),

776fea7123f81f902b0a4c2ff987c0b1db5e7c4f
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-07-02 13:48:33 +0200

    feat: Implementiere Routing und Seitenkomponenten für die Anwendung

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { HomePage, CreatePage, NoteDetailPage } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="notes/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
   )
 }
 

c310d1c5f6328e9b0b49270cc475a3bae2697c15
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-07-02 14:01:20 +0200

    feat: Füge Toast-Benachrichtigung und Schaltfläche zur App hinzu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { HomePage, CreatePage, NoteDetailPage } from './pages'
+import toast from 'react-hot-toast'
 
 const App = () => {
   return (
     <div>
+      <button onClick={() => toast.success('baNaNa')}>Click me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />

bc6c93e3aa686910237871cc8292e656e53c19f8
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-07-04 16:46:42 +0200

    feat: add daisyUI for theming and update App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { HomePage, CreatePage, NoteDetailPage } from './pages'
-import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>
-      <button onClick={() => toast.success('baNaNa')} className="text-red-500 py-5 cursor-pointer ">Click me</button>
+    <div data-theme="forest"> 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />

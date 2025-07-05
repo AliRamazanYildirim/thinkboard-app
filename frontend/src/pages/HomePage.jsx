79e2de2b9ff7ce46661acca02ca40a71b74bd8cb
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-07-05 15:48:36 +0200

    feat: Implementiere RateLimitedUi-Komponente und aktualisiere HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
 import NavBar from '../components/NavBar'
+import RateLimitedUi from '../components/RateLimitedUi';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true);
+
   return (
     <div className='min-h-screen'>
       <NavBar />
+      {isRateLimited && <RateLimitedUi /> }
     </div>
   )
 }

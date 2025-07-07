64583c6baef704d68d7673037b36ede7db0be8ad
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-07-07 15:16:15 +0200

    feat: Aktualisiere NavBar-Komponente zur Verbesserung des Hintergrunds und der Textfarbe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,15 @@ import { HomePage, CreatePage, NoteDetailPage } from './pages'
 
 const App = () => {
   return (
-    <div data-theme="forest"> 
+    <div className="relative min-h-screen w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-[radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="notes/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,17 +2,19 @@ import { Link } from "react-router";
 import { PlusIcon } from "lucide-react";
 const NavBar = () => {
   return (
-    <header className="bg-base-300 border-b border-base-content/10">
+    <header 
+      className="relative border-b border-white/10 bg-[radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]"
+    >
       <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
             <a
               href="/"
-              className="text-lg font-bold text-orange-600 tracking-tight"
+              className="text-lg font-bold text-white tracking-tight"
             >
               ThinkBoard
             </a>
             <div className="flex items-center space-x-2">
-              <Link to={"/create"} className="btn btn-success">
+              <Link to={"/create"} className="btn btn-success text-black">
                 <PlusIcon className="size-5" />
                 <span>New Note</span>
               </Link>

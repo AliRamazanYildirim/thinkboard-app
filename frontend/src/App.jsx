0c1aec819463261f416ca69414fa952b9deca93c
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-07-10 21:00:06 +0200

    feat: Füge ConsoleSignature-Komponente hinzu, um eine visuelle Signatur im Konsolenprotokoll anzuzeigen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
-import { HomePage, CreatePage, NoteDetailPage } from './pages'
+import { Routes, Route } from "react-router-dom";
+import { HomePage, CreatePage, NoteDetailPage } from "./pages";
+import ConsoleSignature from "./components/ConsoleSignature";
 
 const App = () => {
   return (
-    <div className="relative min-h-screen w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-[radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="note/:id" element={<NoteDetailPage />} />
-      </Routes>
-    </div>
+    <>
+      <ConsoleSignature />
+      <div className="relative min-h-screen w-full">
+        <div className="absolute inset-0 -z-10 h-full w-full bg-[radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="note/:id" element={<NoteDetailPage />} />
+        </Routes>
+      </div>
+    </>
   );
-}
+};
 
-export default App
+export default App;
